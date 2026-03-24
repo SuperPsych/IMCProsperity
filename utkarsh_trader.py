f2a53c86af0fa0a3bf8f9966092ab53ec39f458d
Utkarsh Goyal <ut2310@gmail.com>
2026-03-24 15:15:23 -0400

    brendan hi

--- utkarsh_trader.py
+++ utkarsh_trader.py
@@ -1,5 +1,6 @@
 from datamodel import OrderDepth, TradingState, Order
 from typing import List, Dict, Any
+import json
 
 
 class Logger:
@@ -22,19 +23,19 @@ class Logger:
         ]))
         self.logs = ""
 
-logger = Logger()
 
+logger = Logger()
 class Trader:
 
     POSITION_LIMIT = {
-        "EMERALDS": 35,
-        "TOMATOES": 35
+        "EMERALDS": 80,
+        "TOMATOES": 80
     }
 
     def run(self, state: TradingState):
+        result: Dict[str, List[Order]] = {}
         logger.print("timestamp:", state.timestamp)
         logger.print("positions:", state.position)
-        result: Dict[str, List[Order]] = {}
 
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
@@ -46,28 +47,27 @@ class Trader:
                 fair = 10000
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-
                 if best_ask == fair and position<0:
-                    orders.append(Order(product, best_ask, -position))
+                    orders.append(Order(product, best_ask, position))
 
                 elif best_bid == fair and position>0:
-                    orders.append(Order(product, best_bid, -position))
+                    orders.append(Order(product, best_bid, position))
 
                 if position < self.POSITION_LIMIT[product] and best_bid<fair:
                     orders.append(Order(product, best_bid+1, self.POSITION_LIMIT[product]-position))
 
                 if position > -self.POSITION_LIMIT[product] and best_ask>fair:
-                    orders.append(Order(product, best_ask-1, -position-self.POSITION_LIMIT[product]))
+                    orders.append(Order(product, best_ask-1, self.POSITION_LIMIT[product]-position))
 
             if product == "TOMATOES":
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
 
                 if position < self.POSITION_LIMIT[product]:
-                    orders.append(Order(product, best_bid + 1, 10))
+                    orders.append(Order(product, best_bid + 1, 5))
 
                 if position > -self.POSITION_LIMIT[product]:
-                    orders.append(Order(product, best_ask - 1, -10))
+                    orders.append(Order(product, best_ask - 1, -5))
 
             result[product] = orders
 
