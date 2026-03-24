31f62a76665b263d94c8ecec275c2c51ab456e73
Brendan Malaugh <superpsych10@gmail.com>
2026-03-24 02:55:27 -0400

    possibly an overfit but it good

--- trader.py
+++ trader.py
@@ -43,14 +43,45 @@ class Trader:
                     orders.append(Order(product, best_ask-1, -position-self.POSITION_LIMIT[product]))
 
             if product == "TOMATOES":
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                buy_orders = dict(sorted(order_depth.buy_orders.items(), reverse=True))
+                sell_orders = dict(sorted(order_depth.sell_orders.items()))
 
-                if position < self.POSITION_LIMIT[product]:
-                    orders.append(Order(product, best_bid + 1, self.POSITION_LIMIT[product]-position))
+                bid_prices = list(buy_orders.keys())
+                ask_prices = list(sell_orders.keys())
 
-                if position > -self.POSITION_LIMIT[product]:
-                    orders.append(Order(product, best_ask - 1, -position-self.POSITION_LIMIT[product]))
+                best_bid = bid_prices[0]
+                best_ask = ask_prices[0]
+
+                if len(bid_prices) > 1 and buy_orders[bid_prices[1]] >  0.77*buy_orders[best_bid]:
+                    bid_wall = bid_prices[1]
+                else:
+                    bid_wall = best_bid
+
+                if len(ask_prices) > 1 and sell_orders[ask_prices[1]] > 0.77*sell_orders[best_ask]:
+                    ask_wall = ask_prices[1]
+                else:
+                    ask_wall = best_ask
+
+                fair = (bid_wall + ask_wall) / 2
+
+                LIMIT = self.POSITION_LIMIT[product]
+
+                for ask, ask_vol in sell_orders.items():
+                    if ask < fair and position < LIMIT:
+                        vol = min(-ask_vol, LIMIT - position)
+                        orders.append(Order(product, ask, vol))
+                        position += vol
+
+                for bid, bid_vol in buy_orders.items():
+                    if bid > fair and position > -LIMIT:
+                        vol = min(bid_vol, LIMIT + position)
+                        orders.append(Order(product, bid, -vol))
+                        position -= vol
+
+                if position < LIMIT:
+                    orders.append(Order(product, best_bid + 1, LIMIT - position))
+                if position > -LIMIT:
+                    orders.append(Order(product, best_ask - 1, -LIMIT - position))
 
             result[product] = orders
 
