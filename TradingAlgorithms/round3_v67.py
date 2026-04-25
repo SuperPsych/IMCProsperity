06506669a7c180878a443a7cbe465ad03974efae
unknown <vincent.k.trang@gmail.com>
2026-04-25 19:02:22 -0400

    SIX SEVENNNN

--- TradingAlgorithms/round3_v67.py
+++ TradingAlgorithms/round3_v67.py
@@ -79,7 +79,7 @@ def sell(product: str, price: int, quantity: int) -> Order:
 PARAMS = {
     "hydrogel_mean": 9990,
     "hydrogel_alpha": 300.0,
-    "hydrogel_offset": 29,
+    "hydrogel_offset": .9*31,
 
     "velvetfruit_mean": 5255,
     "velvetfruit_alpha": 300.0,
@@ -87,14 +87,14 @@ PARAMS = {
 
     "VEV_4000_mean": 1250.0, "VEV_4000_alpha": 300.0, "VEV_4000_offset": 15.6,
     "VEV_4500_mean": 750.0,  "VEV_4500_alpha": 300.0, "VEV_4500_offset": 15.6,
-    "VEV_5000_mean": 255.0,  "VEV_5000_alpha": 300.0, "VEV_5000_offset": 14.4,
-    "VEV_5100_mean": 167.2,  "VEV_5100_alpha": 300.0, "VEV_5100_offset": 12.7,
-    "VEV_5200_mean": 98.1,   "VEV_5200_alpha": 300.0, "VEV_5200_offset": 9.7,
-    "VEV_5300_mean": 50.0,   "VEV_5300_alpha": 300.0, "VEV_5300_offset": 6.2,
-    "VEV_5400_mean": 19.7,   "VEV_5400_alpha": 300.0, "VEV_5400_offset": 3.4,
-    "VEV_5500_mean": 8.7,    "VEV_5500_alpha": 300.0, "VEV_5500_offset": 1.7,
-    "VEV_6000_mean": 0.5,    "VEV_6000_alpha": 300.0, "VEV_6000_offset": 0.0,
-    "VEV_6500_mean": 0.5,    "VEV_6500_alpha": 300.0, "VEV_6500_offset": 0.0,
+    "VEV_5000_mean": 255.0,  "VEV_5000_alpha": 300.0, "VEV_5000_offset": .67*14.4,
+    "VEV_5100_mean": 167.2,  "VEV_5100_alpha": 300.0, "VEV_5100_offset": .67*12.7,
+    "VEV_5200_mean": 98.1,   "VEV_5200_alpha": 300.0, "VEV_5200_offset": .67*9.7,
+    "VEV_5300_mean": 50.0,   "VEV_5300_alpha": 300.0, "VEV_5300_offset": .67*6.2,
+    "VEV_5400_mean": 19.7,   "VEV_5400_alpha": 300.0, "VEV_5400_offset": .67*3.4,
+    "VEV_5500_mean": 8.7,    "VEV_5500_alpha": 300.0, "VEV_5500_offset": .67*1.7,
+    "VEV_6000_mean": 0.5,    "VEV_6000_alpha": 300.0, "VEV_6000_offset": .67*0.0,
+    "VEV_6500_mean": 0.5,    "VEV_6500_alpha": 300.0, "VEV_6500_offset": .67*0.0,
 
     "ewma_gamma": 1,
     "ewma_gamma_opts": 1,
